5bde171117d1d87da9af61017f1b86d6e1894bde
gabriele92984 <60701037+gabriele92984@users.noreply.github.com>
2024-12-28 17:29:04 +0100

    Lint by RuboCop

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,7 +1,7 @@
-module Enumerable
+module Enumerable # rubocop:disable Style/Documentation
   def my_map
     new_array = []
-    self.my_each do |item| 
+    my_each do |item|
       new_array << yield(item)
     end
 
@@ -10,70 +10,63 @@ module Enumerable
 
   def my_select
     new_array = []
-    self.my_each do |item|
+    my_each do |item|
       new_array << item if yield(item)
-      end
-
+    end
     new_array
   end
 
   def my_each_with_index
     index = 0
-    self.my_each do |item|
+    my_each do |item|
       yield(item, index) if yield(item, index) == true
       index += 1
     end
-
     self
   end
 
   def my_all?
-    self.my_each do |item|
+    my_each do |item|
       return false unless yield(item)
     end
-
     true
   end
 
   def my_none?
-    self.my_each do |item|
+    my_each do |item|
       return false if yield(item)
     end
-
     true
   end
 
   def my_any?
-    self.my_each do |item|
+    my_each do |item|
       return true if yield(item)
     end
-
     false
   end
 
   def my_count
     if block_given?
       count = 0
-      self.my_each do |item|
-        count += 1 if yield(item)  # Increment count if the block condition is met
+      my_each do |item|
+        count += 1 if yield(item) # Increment count if the block condition is met
       end
-
-      count  # Return the total count of items satisfying the condition
+      count # Return the total count of items satisfying the condition
     else
-
-      self.size  # Return the size of the enumerable if no block is given
+      size # Return the size of the enumerable if no block is given
     end
   end
 
   def my_inject(initial_value = nil)
     memo = initial_value
 
-    my_each do |item|
-      if memo.nil?
-        memo = item 
-      else
-        memo = yield(memo, item)
-      end
+    each do |item|
+      memo = if memo.nil?
+               item
+             else
+               yield(memo, item)
+             end
     end
 
     memo
@@ -85,10 +78,8 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  def my_each
-    for item in self
-      yield(item)
-    end
+  def my_each(&)
+    each(&block)
 
     self
   end
